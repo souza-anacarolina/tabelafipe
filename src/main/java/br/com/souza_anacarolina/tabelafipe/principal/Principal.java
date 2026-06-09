1e841c2e3490ce75010ab01e759cceae3af90097
Ana <souzacarol874@gmail.com>
2026-06-09 18:54:00 -0300

    Ordenando os resultados do array retornando e tratando parametros incorretos repassados para a api

--- src/main/java/br/com/souza_anacarolina/tabelafipe/principal/Principal.java
+++ src/main/java/br/com/souza_anacarolina/tabelafipe/principal/Principal.java
@@ -6,7 +6,11 @@ import br.com.souza_anacarolina.tabelafipe.service.ConverteDados;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
 
@@ -16,17 +20,36 @@ public class Principal {
 
     private ConverteDados converteDados = new ConverteDados();
 
-    private final String ENDERECO = "https://parallelum.com.br/fipe/api/v1";
+    private final String ENDERECO = "https://parallelum.com.br/fipe/api/v1/";
 
-    public void exibeMenu(){
-        System.out.println("OPÇÕES DISPONÍVEIS PARA CONSULTA\n" +
+    public void exibeMenu() {
+        System.out.println("OPÇÕES DISPONÍVEIS PARA CONSULTA\n\n" +
                 "CARRO\n" +
                 "CAMINHÃO\n" +
                 "MOTO\n\n" +
                 "Com qual opção gostaria de prosseguir?");
 
-        var tipo = URLEncoder.encode(scanner.nextLine(), StandardCharsets.UTF_8);
-        var json = consumoApi.obterDados(ENDERECO + tipo);
+        var tipo = URLEncoder.encode(scanner.nextLine().toLowerCase(), StandardCharsets.UTF_8);
+
+        if (tipo.equals("carro")) tipo = "carros";
+        if (tipo.equals("moto")) tipo = "motos";
+        if (tipo.equals("caminhão") || tipo.equals("caminhao")) tipo = "caminhoes";
+        var json = consumoApi.obterDados(ENDERECO + tipo + "/marcas");
+
+        DadosVeiculo[] marcas = converteDados.obterDados(json, DadosVeiculo[].class);
+
+        List<DadosVeiculo> marcasOrdenadas = Arrays.stream(marcas)
+                .sorted(Comparator.comparing(DadosVeiculo::codigo))
+                .collect(Collectors.toList());
+
+        System.out.println("\nMARCAS DISPONÍVEIS PARA CONSULTA\n");
+        for (DadosVeiculo m : marcasOrdenadas) {
+            System.out.println(m.codigo() + " - " + m.nome());
+        }
+
+        System.out.println("De qual marca?");
+        var marca = URLEncoder.encode(scanner.nextLine().toLowerCase(), StandardCharsets.UTF_8);
+
         DadosVeiculo dadosVeiculo = converteDados.obterDados(json, DadosVeiculo.class);
         System.out.println(dadosVeiculo);
 
